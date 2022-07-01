603f388394ced2814b75fcdc90168fe98ac84e6b
WFS <201250185@smail.nju.edu.cn>
2022-07-01 18:05:47 +0800

    add some 注释 of PayMoneyController

--- src/main/java/com/nju/edu/erp/web/controller/finance/PayMoneyController.java
+++ src/main/java/com/nju/edu/erp/web/controller/finance/PayMoneyController.java
@@ -8,10 +8,10 @@ import com.nju.edu.erp.model.vo.finance.PayMoneySheetVO;
 import com.nju.edu.erp.service.PayMoneyService;
 import com.nju.edu.erp.web.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/payMoney")
 public class PayMoneyController {
     private final PayMoneyService payMoneyService;
 
