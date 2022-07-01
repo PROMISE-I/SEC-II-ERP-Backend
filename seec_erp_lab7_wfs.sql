8ba8f019772e68b16e381b38d0d2002b99a47558
WFS <201250185@smail.nju.edu.cn>
2022-07-01 16:43:33 +0800

    定义了receive_money_sheet表

--- seec_erp_lab7_wfs.sql
+++ seec_erp_lab7_wfs.sql
@@ -9,3 +9,17 @@ CREATE TABLE `bank_account`  (
      PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for receive_money_sheet
+-- ----------------------------
+DROP TABLE IF EXISTS `receive_money_sheet`;
+CREATE TABLE `receive_money_sheet`  (
+     `id` varchar(31) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '收款单单据编号（格式为：SKD-yyyyMMdd-xxxxx',
+     `customer` int(11) NULL DEFAULT NULL COMMENT '客户（包括供应商和销售商）',
+     `operator` varchar(31) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '操作员',
+     `total_amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '转账总额',
+     `state` varchar(31) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '单据状态',
+     `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+     PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
