1220fe25f04c213562b8a7118bcfe305e2505625
SUNLIFAN <1656680420@qq.com>
2022-07-01 23:41:04 +0800

    OperateSheetServiceImpl init, to be done

---
+++ src/main/java/com/nju/edu/erp/service/Impl/OperateSheetServiceImpl.java
@@ -0,0 +1,29 @@
+package com.nju.edu.erp.service.Impl;
+
+import com.nju.edu.erp.service.OperateSheetService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class OperateSheetServiceImpl implements OperateSheetService {
+    @Override
+    public BigDecimal calculateFinalIncome(String begin, String end) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal calculateDiscountedAmount(String begin, String end) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal calculateCost(String begin, String end) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal calculateProfit(String begin, String end) {
+        return null;
+    }
+}
