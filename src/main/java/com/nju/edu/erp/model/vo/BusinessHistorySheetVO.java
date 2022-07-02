62a15860b57908a2a94754b5e6509f502736c8a3
SUNLIFAN <1656680420@qq.com>
2022-07-02 22:41:02 +0800

    mock implementation for BusinessHistoryController now, to be done

---
+++ src/main/java/com/nju/edu/erp/model/vo/BusinessHistorySheetVO.java
@@ -0,0 +1,28 @@
+package com.nju.edu.erp.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BusinessHistorySheetVO {
+    /**
+     * 单据类型:
+     * 1. 销售类（销售出货单，销售退货单）
+     * 2. 财务类（付款单，收款单，工资单）
+     * 3. 进货类（进货单，进货退货单）
+     * 4. 库存类（赠送单）
+     */
+    public String type;
+
+    /**
+     * 单据内容
+     */
+    Object sheetObject;
+
+}
