ac311663323a2a34eb2f014d6ffa3470c51ec11b
WFS <201250185@smail.nju.edu.cn>
2022-07-03 10:56:40 +0800

    add some util of date for salary control

--- src/main/java/com/nju/edu/erp/utils/DateHelper.java
+++ src/main/java/com/nju/edu/erp/utils/DateHelper.java
@@ -1,5 +1,8 @@
 package com.nju.edu.erp.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.spi.DateFormatProvider;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -15,5 +18,31 @@ public class DateHelper {
         calendar.set(Calendar.MILLISECOND, 0);
 
         return calendar.getTime();
+    }
+
+    public static boolean isGMSalaryDay() {
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return month == Calendar.DECEMBER && day == 1;
+    }
+
+    public static boolean isSalaryDay() {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        return day == 1;
+    }
+
+    public static boolean isSameAboveMonth(Date date1, Date date2) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date1);
+        int year1 = calendar.get(Calendar.YEAR);
+        int month1 = calendar.get(Calendar.MONTH);
+
+        calendar.setTime(date2);
+        int year2 = calendar.get(Calendar.YEAR);
+        int month2 = calendar.get(Calendar.MONTH);
+
+        return year1 == year2 && month1 == month2;
     }
 }
