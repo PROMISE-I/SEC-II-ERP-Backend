47f0849b5550edd27bc6b3af150ad401eef1054e
WFS <201250185@smail.nju.edu.cn>
2022-07-03 10:11:08 +0800

    add some comments

--- src/main/java/com/nju/edu/erp/service/SalaryService.java
+++ src/main/java/com/nju/edu/erp/service/SalaryService.java
@@ -6,9 +6,22 @@ import com.nju.edu.erp.model.vo.finance.SalarySheetVO;
 import java.util.List;
 
 public interface SalaryService {
+    /**
+     * 制定工资单
+     * @param employeeId 员工id
+     * @param bankAccountId 员工银行账户id
+     */
     void makeSalarySheet(int employeeId, int bankAccountId);
 
+    /**
+     * 根据员工id返回当月的薪资
+     * @param employeeId 员工id
+     */
     int getSalaryByEmployeeId(int employeeId);
 
+    /**
+     * 根据状态返回相应的工资单
+     * @param state 工资单状态
+     */
     List<SalarySheetVO> getSalarySheetByState(SalarySheetState state);
 }
