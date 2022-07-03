2846b4c448ce031637d9917a96ed22a69e22eac6
WFS <201250185@smail.nju.edu.cn>
2022-07-03 18:11:34 +0800

    完成部分工资单制定模块的测试文件

---
+++ src/test/java/com/nju/edu/erp/service/SalaryServiceTest.java
@@ -0,0 +1,35 @@
+package com.nju.edu.erp.service;
+
+import com.nju.edu.erp.enums.sheetState.SalarySheetState;
+import com.nju.edu.erp.model.vo.finance.SalarySheetVO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class SalaryServiceTest {
+
+    @Autowired
+    SalaryService salaryService;
+
+    @Autowired
+    BankAccountService bankAccountService;
+
+    @Test
+    public void sheetMake() {
+        salaryService.makeSalarySheet(0, 0);
+        salaryService.approval("GZD-202207-0", SalarySheetState.PENDING_LEVEL_2);
+        salaryService.approval("GZD-202207-0", SalarySheetState.SUCCESS);
+        BigDecimal amount = bankAccountService.getAmountByAccountId(0);
+        List<SalarySheetVO> sheets = salaryService.getSalarySheetByState(SalarySheetState.PENDING_LEVEL_1);
+
+        System.out.println(sheets);
+    }
+}
