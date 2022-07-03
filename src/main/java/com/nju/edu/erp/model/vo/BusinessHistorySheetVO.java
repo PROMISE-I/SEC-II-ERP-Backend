030f39a381d37ae3ceb2d7d539c81558282f813d
SUNLIFAN <1656680420@qq.com>
2022-07-03 16:16:55 +0800

    business-history part v1 mostly implemented

--- src/main/java/com/nju/edu/erp/model/vo/BusinessHistorySheetVO.java
+++ src/main/java/com/nju/edu/erp/model/vo/BusinessHistorySheetVO.java
@@ -13,10 +13,10 @@ import lombok.NoArgsConstructor;
 public class BusinessHistorySheetVO {
     /**
      * 单据类型:
-     * 1. 销售类（销售出货单，销售退货单）
-     * 2. 财务类（付款单，收款单，工资单）
-     * 3. 进货类（进货单，进货退货单）
-     * 4. 库存类（赠送单）
+     * 1. 销售类（销售出货单，销售退货单）: sale, sale-returns
+     * 2. 财务类（付款单，收款单，工资单）: pay-money, receive-money, salary(todo)
+     * 3. 进货类（进货单，进货退货单）: purchase, purchase-returns
+     * 4. 库存类（赠送单）: present(todo)
      */
     public String type;
 
--- src/main/java/com/nju/edu/erp/service/BusinessHistoryService.java
+++ src/main/java/com/nju/edu/erp/service/BusinessHistoryService.java
@@ -1,5 +1,9 @@
 package com.nju.edu.erp.service;
 
+import com.nju.edu.erp.model.vo.BusinessHistorySheetVO;
+
+import java.util.List;
+
 public interface BusinessHistoryService {
-    
+    public List<BusinessHistorySheetVO> findALl(String begin, String end);
 }
