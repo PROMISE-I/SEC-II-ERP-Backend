61924801e0a70dc6796799fa9f3eea75117e6ec0
WFS <201250185@smail.nju.edu.cn>
2022-07-08 11:04:20 +0800

    define year end awards vo/po

---
+++ src/main/java/com/nju/edu/erp/model/po/finance/YearEndAwardsPO.java
@@ -0,0 +1,33 @@
+package com.nju.edu.erp.model.po.finance;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author WFS
+ * @date 2022/7/8 11:03
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class YearEndAwardsPO {
+    /**
+     * 员工id
+     */
+    private Integer staffId;
+
+    /**
+     * 年份
+     */
+    private Integer year;
+
+    /**
+     * 年终奖总额
+     */
+    private BigDecimal amount;
+}
