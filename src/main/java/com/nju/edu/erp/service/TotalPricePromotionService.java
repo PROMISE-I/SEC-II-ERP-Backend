abb01ed95eebbdd35b0a2da48ce8c6c494f6f6f2
WFS <201250185@smail.nju.edu.cn>
2022-07-08 20:24:55 +0800

    add some comments

--- src/main/java/com/nju/edu/erp/service/TotalPricePromotionService.java
+++ src/main/java/com/nju/edu/erp/service/TotalPricePromotionService.java
@@ -10,13 +10,36 @@ import java.util.List;
  * @date 2022/7/8 19:29
  */
 public interface TotalPricePromotionService {
+    /**
+     * 总经理制定一个总价的促销策略
+     * @param userVO
+     * @param totalPricePromotionVO
+     */
     void makePromotion(UserVO userVO, TotalPricePromotionVO totalPricePromotionVO);
 
+    /**
+     * 更新一个总价的促销策略
+     * @param userVO
+     * @param totalPricePromotionVO
+     */
     void updatePromotion(UserVO userVO, TotalPricePromotionVO totalPricePromotionVO);
 
+    /**
+     * 获取所有总价的促销策略
+     * @return
+     */
     List<TotalPricePromotionVO> findAll();
 
+    /**
+     * 根据促销策略id设置总价的促销策略
+     * @param id
+     * @return
+     */
     TotalPricePromotionVO findById(String id);
 
+    /**
+     * 根据促销策略id伤处一个总价的促销策略
+     * @param id
+     */
     void deleteById(String id);
 }
