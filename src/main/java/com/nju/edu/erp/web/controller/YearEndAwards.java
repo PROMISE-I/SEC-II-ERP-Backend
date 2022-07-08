e542dd25125be564d75ea87295231c7db1af738c
WFS <201250185@smail.nju.edu.cn>
2022-07-08 10:03:48 +0800

    add function "year end awards" in controller

--- src/main/java/com/nju/edu/erp/web/controller/YearEndAwards.java
+++ src/main/java/com/nju/edu/erp/web/controller/YearEndAwards.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/year-end-awards")
 public class YearEndAwards {
 
-    private YearEndAwardsService yearEndAwardsService;
+//    private YearEndAwardsService yearEndAwardsService;
 
     @PostMapping("/sheet-make")
     public Response makeSheet() {
