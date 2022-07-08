f695bdc0b18a5e2d175c11a5335e53ebd07e476a
WFS <201250185@smail.nju.edu.cn>
2022-07-08 18:10:54 +0800

    add function: 面向接口变成的单据业务逻辑层重构

---
+++ src/main/java/com/nju/edu/erp/service/sheet/SheetService.java
@@ -0,0 +1,15 @@
+package com.nju.edu.erp.service.sheet;
+
+import com.nju.edu.erp.model.vo.UserVO;
+
+/**
+ * @author WFS
+ * @date 2022/7/8 17:56
+ */
+public interface SheetService {
+//    void makeSheet(UserVO userVO, SheetVO sheetVO);
+//
+//    List<SheetVO> getSheetByState(SheetState state);
+//
+//    void approval
+}
