8b9da5999e7136b5c4e6fd31bf045c9c11b293ed
WFS <201250185@smail.nju.edu.cn>
2022-07-09 17:23:22 +0800

    completed business logic of "give away" module

---
+++ src/main/java/com/nju/edu/erp/dao/promotion/GiveAwayDao.java
@@ -0,0 +1,35 @@
+package com.nju.edu.erp.dao.promotion;
+
+import com.nju.edu.erp.enums.sheetState.GiveAwaySheetState;
+import com.nju.edu.erp.model.po.promotion.GiveAwaySheetContentPO;
+import com.nju.edu.erp.model.po.promotion.GiveAwaySheetPO;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author WFS
+ * @date 2022/7/9 17:21
+ */
+@Mapper
+@Repository
+public interface GiveAwayDao {
+    GiveAwaySheetPO getLatestSheet();
+
+    void save(GiveAwaySheetPO toSave);
+
+    void saveBatch(List<GiveAwaySheetContentPO> contentPOS);
+
+    List<GiveAwaySheetPO> findAll();
+
+    List<GiveAwaySheetPO> findByState(GiveAwaySheetState state);
+
+    GiveAwaySheetPO findSheetById(String sheetId);
+
+    int updateSheetState(GiveAwaySheetPO sheet, GiveAwaySheetPO sheet1);
+
+    int updateSheetStateOnPrev(String sheetId, GiveAwaySheetState prevState, GiveAwaySheetState state);
+
+    List<GiveAwaySheetContentPO> findContentById(String sheetId);
+}
--- src/main/java/com/nju/edu/erp/model/vo/promotion/GiveAwaySheetContentVO.java
+++ src/main/java/com/nju/edu/erp/model/vo/promotion/GiveAwaySheetContentVO.java
@@ -1,11 +1,20 @@
 package com.nju.edu.erp.model.vo.promotion;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 /**
  * @author WFS
  * @date 2022/7/9 10:53
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class GiveAwaySheetContentVO {
     /**
      * 自增id
